aabb8a9396172f8ea4c8b37052a16db8c12bdbb8
Alexander <sany199574@gmail.com>
2023-06-06 20:42:11 +0300

    Решил задача RangeSum

--- RangeSum/Program.cs
+++ RangeSum/Program.cs
@@ -5,28 +5,77 @@
 //расположенных между данными числами на экран. Дано два числа A и B (A<B)
 //выведите все нечетные значения, расположенные между данными числами.  
 
+RangeSum();
+Console.ReadLine();
 
-
-
-
-
-
-static int[] DigitsGenerator(int count)
+static void RangeSum()
 {
-    List<int> digits = new List<int>();
+    var digits = Enter();
+    var next = digits.Item1;
+    var last = digits.Item2;
 
-    for (int i = 0; i <= count; i++)
+    List<double> allDigits = new List<double>();
+    double sum = 0;
+
+    for (double i = next + 1; i < last; i++)
     {
-        digits.Add(i);
+        sum += i;
+        allDigits.Add(i);
     }
-    return digits.ToArray();
+    List<double>  noEventNumbers = ParityCheck(allDigits);
+
+    Show(allDigits, "\nЧисла попавшие под диапазон");
+    Show(noEventNumbers, "\nНечётные числа попавшие под диапазон");
+    Console.WriteLine("\nСумма чисел попавших под диапазон: \n{0}", sum);
 }
 
-static void Show(List<int> array)
+static List<double> ParityCheck(List<double> digits)
 {
-    Console.WriteLine("=-----------------------------=");
-    foreach (var item in array)
+    List<double> noEventNumbers = new List<double>();
+    foreach (var item in digits)
     {
-        Console.Write("{0} ", item);
+        if (item % 2 == 0 && item != 0)
+        {
+            noEventNumbers.Add(item);
+        }
+    }
+    return noEventNumbers;
+}
+
+
+static double IsDigit()
+{
+    double digit;
+    bool isDigit = false;
+    do
+    {
+        isDigit = double.TryParse(Console.ReadLine(), out digit);
+    } while (!isDigit);
+    return digit;
+}
+
+static (double, double) Enter()
+{
+    Console.WriteLine("Введите 2 числа");
+    Console.WriteLine("Введите число A");
+    var A = IsDigit();
+    Console.WriteLine("Введите число B");
+    var B = IsDigit();
+    if (A > B)
+    {
+        double a = B;
+        B = A;
+        A = a;
+    }
+
+    return(A, B);
+}
+
+static void Show(List<double> digits, string name)
+{
+    Console.WriteLine(name);
+    foreach (var item in digits)
+    {
+        Console.Write(item + " ");
     }
 }
