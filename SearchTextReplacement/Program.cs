010aedde2841af11783dbbcfb3b20bf04b672ae3
Alexander <sany199574@gmail.com>
2023-06-30 00:16:56 +0300

    Решил SearchTextReplacement

---
+++ SearchTextReplacement/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//Пользователь вводит текст, строку для поиска и строку для замены.
+//Реализовать поиск в тексте заданной строки и замены ее на заданную подстроку. 
+
+
+
+SearchTextReplacement();
+Console.ReadLine();
+
+
+static void SearchTextReplacement()
+{
+    string text;
+    string search;
+    string replace;
+
+    bool isSearch = false;
+
+    //Console.WriteLine("Введите текст");
+    //text = Console.ReadLine();
+    //Console.WriteLine("Введите слово которое хотите заменить");
+    //search = Console.ReadLine();
+    //Console.WriteLine("Введите слово на которое хотите заменить");
+    //replace = Console.ReadLine();
+
+
+    text = "У лукоморья дуб зелёный;\r\nЗлатая цепь на дубе том:\r\nИ днём и ночью кот учёный\r\nВсё ходит по цепи кругом;";
+    search = "цепь";
+    replace = "Цепуха";
+
+    Console.WriteLine(text);
+    Console.WriteLine();
+
+    isSearch = text.Contains(search);
+    if (isSearch)
+    {
+        text = text.Replace(search, replace);
+    }
+
+    Console.WriteLine(text);
+
+}
