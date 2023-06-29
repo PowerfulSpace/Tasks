449d9f4c8a97abd5ca64155be1ba05b5f4603aeb
Alexander <sany199574@gmail.com>
2023-06-29 07:08:07 +0300

    Решил ReadingCharacters

---
+++ ReadingCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//Написать программу, которая считывает символы с клавиатуры, пока не будет введена точка.
+//Программа должна сосчитать количество введенных пользователем пробелов. 
+
+
+
+
+
+ReadingCharacters();
+Console.ReadLine();
+
+static void ReadingCharacters()
+{
+
+	char key = char.MinValue;
+	int countSpace = 0;
+
+	do
+	{
+		key = Console.ReadKey().KeyChar;
+
+		if (key == ' ')
+            countSpace++;
+
+
+    } while (key != '.');
+
+	Console.WriteLine();
+	Console.WriteLine($"Количество пробелов было введено: {countSpace}");
+
+}
+
+
+
+
+
+
