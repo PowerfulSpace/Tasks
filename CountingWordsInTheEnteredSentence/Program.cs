b762c575650d31196f171a1f5be5018757180fab
Alexander <sany199574@gmail.com>
2023-06-30 03:38:57 +0300

    Решил CountingWordsThatEndInAVowel

---
+++ CountingWordsInTheEnteredSentence/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Подсчитать количество слов во введенном предложении. Решить двумя способами.
+
+
+
+
+CountingWordsInTheEnteredSentence();
+Console.ReadLine();
+
+
+static void CountingWordsInTheEnteredSentence()
+{
+    string text = "Подсчитать количество слов во введенном предложении. Решить двумя способами";
+    //string text = Console.ReadLine();
+    string[] words;
+
+    words = text.Replace(".", "").Split(" ");
+
+    Console.WriteLine($"Количество слов в ведённом предлежении: {words.Length}");
+}
+
+
+
+
+
+
+
+
+
---
+++ CountingWordsThatEndInAVowel/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//. Пользователь вводит русский текст. Подсчитать количество слов, которые заканчиваются на гласную букву.
+
+
+
+CountingWordsThatEndInAVowel();
+Console.ReadLine();
+
+
+static void CountingWordsThatEndInAVowel()
+{
+    char[] vowels = new char[] { 'а','и','е','ё','о','у','ы','э','ю','я'};
+    string text = "Пользователь вводит русский текст. Подсчитать количество слов, которые заканчиваются на гласную букву";
+    
+    string[] allWords;
+    List<string> wordsEndingInAVowel = new List<string>();
+
+    allWords = text.Replace(".", "").Split(" ");
+
+    foreach (var word in allWords)
+    {
+        for (int i = 0; i < vowels.Length; i++)
+        {
+            if (word.EndsWith(vowels[i]))
+            {
+                wordsEndingInAVowel.Add(word);
+                break;
+            }
+        } 
+    }
+    Console.WriteLine("Все слова которые заканчиваются на гласную букву");
+    Console.WriteLine();
+    foreach (var item in wordsEndingInAVowel)
+    {
+        Console.Write(item + " ");
+    }
+}
