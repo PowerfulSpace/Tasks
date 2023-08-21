10c73ff5a1f4f408adc52b392134e996642ad2a5
Alexander <sany199574@gmail.com>
2023-08-21 06:56:25 +0300

    Решил задание где нужно было написать дешифратор. Где все предолги бы заменяли на слово «Hello!»

--- ReplacingPrepositionsInTheText/Program.cs
+++ ReplacingPrepositionsInTheText/Program.cs
@@ -9,13 +9,13 @@ using System.Text.RegularExpressions;
 string patch = @"D:\Testing\Test.txt";
 using StreamReader reader = new StreamReader(patch);
 string text = reader.ReadToEnd();
-string[] prepositions = new string[] {"в","без","до","для","за","через","над","по","из","у","около","под","о","про","на","к","между","перед","при","с","между" };
+string[] prepositions = new string[] { "в", "без", "до", "для", "за", "через", "над", "по", "из", "у", "около", "под", "о", "про", "на", "к", "между", "перед", "при", "с", "между" };
 
 string pattert = @"\b(в|между|без|до|для|за|через|над|по|из|у|около|под|о|про|на|к|между|перед|при|с|между)\b";
 string pattertReplace = @"«Hello!»";
 Regex regex = new Regex(pattert);
 
-var result = regex.Replace(text,pattertReplace);
+var result = regex.Replace(text, pattertReplace);
 
 Console.WriteLine(result);
 
